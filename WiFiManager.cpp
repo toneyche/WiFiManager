fd326abb24a15912bdfd0d098a512bf6d50ed640
toneyche <43717356+toneyche@users.noreply.github.com>
2024-03-18 15:12:04 -0700

    Update WiFiManager.cpp
    add function show temp

--- WiFiManager.cpp
+++ WiFiManager.cpp
@@ -2014,7 +2014,10 @@ void WiFiManager::handleInfo() {
       F("apssid"),
       F("apip"),
       F("apbssid"),
-      F("apmac")
+      F("apmac"),
+    #ifdef HAVE_DS18B20	
+      F("temperature")
+    #endif
     };
 
   #elif defined(ESP32)
@@ -2311,6 +2314,13 @@ String WiFiManager::getInfoData(String id){
     p = FPSTR(HTTP_INFO_aboutdate);
     p.replace(FPSTR(T_1),String(__DATE__ " " __TIME__));
   }
+  #ifdef HAVE_DS18B20
+  else if(id==F("temperature")){
+  p = F("Temperature: ");
+  p += String(fahrenheit, 2); // 假设fahrenheit是一个float类型的变量
+  p += F(" °F");
+  }
+  #endif
   return p;
 }
 
